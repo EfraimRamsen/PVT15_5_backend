bce9c6e24231046866ce6a1d1ef8b9aff4222128
Jocke007 <jocke.ke@gmail.com>
2019-04-15 14:02:24 +0200

    Moved .gitignore file

--- helloworld/src/main/java/se/su/dsv/pvt/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/se/su/dsv/pvt/helloworld/HelloworldApplication.java
@@ -2,6 +2,9 @@ package se.su.dsv.pvt.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class HelloworldApplication {
@@ -9,5 +12,14 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
+
+	@RestController
+    public class helloRestController{
+	    @RequestMapping("/sayHello")
+        public String greeting(){
+	        return "Hello, it works!";
+        }
+
+    }
 
 }
