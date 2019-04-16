529d191ceab6319da3c673fb47f135091ccc5c75
Jocke007 <jocke.ke@gmail.com>
2019-04-16 10:30:02 +0200

    Add Razans code to the HelloworldApplication.java file

--- helloworld/src/main/java/se/su/dsv/pvt/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/se/su/dsv/pvt/helloworld/HelloworldApplication.java
@@ -1,19 +1,42 @@
 package se.su.dsv.pvt.helloworld;
 
+//@SpringBootApplication
+//public class HelloworldApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(HelloworldApplication.class, args);
+//	}
+//
+//	@RestController
+//    public class helloRestController{
+//	    @RequestMapping("/sayHello")
+//        public String greeting(){
+//	        return "Hello, it works!";
+//
+//        }
+//
+//    }
+//
+//}
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloworldApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
-	}
-
-	@RestController
+    	@RestController
     public class helloRestController{
 	    @RequestMapping("/sayHello")
         public String greeting(){
@@ -22,5 +45,5 @@ public class HelloworldApplication {
         }
 
     }
-
 }
+
