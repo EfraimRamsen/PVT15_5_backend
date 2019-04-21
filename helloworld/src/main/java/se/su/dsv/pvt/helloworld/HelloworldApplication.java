7197daa872c01331bf5958f552c38e9be089d88c
jbar <mr.jockex09@hotmail.com>
2019-04-21 12:44:17 +0200

    Testar att ta bort getters och setters i StringResponse-klassen för att se om JSON-returnering fortfarande fungerar.

--- helloworld/src/main/java/se/su/dsv/pvt/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/se/su/dsv/pvt/helloworld/HelloworldApplication.java
@@ -83,29 +83,29 @@ public class HelloworldApplication extends SpringBootServletInitializer {
 				this.age = age;
 			}
 
-			public String getResponse() {
-				return response;
-			}
-
-			public void setResponse(String response) {
-				this.response = response;
-			}
-
-			public String getName() {
-				return name;
-			}
-
-            public void setName(String name) {
-                this.name = name;
-            }
-
-            public String getAge() {
-				return age;
-			}
-
-            public void setAge(String age) {
-                this.age = age;
-            }
+//			public String getResponse() {
+//				return response;
+//			}
+//
+//			public void setResponse(String response) {
+//				this.response = response;
+//			}
+//
+//			public String getName() {
+//				return name;
+//			}
+//
+//            public void setName(String name) {
+//                this.name = name;
+//            }
+//
+//            public String getAge() {
+//				return age;
+//			}
+//
+//            public void setAge(String age) {
+//                this.age = age;
+//            }
         }
 	}
 
