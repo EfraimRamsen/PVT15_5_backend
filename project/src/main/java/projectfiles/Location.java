726ad3ec64c467347a5f1c36fa5cc8846db0f442
Jocke007 <jocke.ke@gmail.com>
2019-05-02 14:36:25 +0200

    Added parameters to location

--- project/src/main/java/projectfiles/Location.java
+++ project/src/main/java/projectfiles/Location.java
@@ -4,4 +4,12 @@ package projectfiles;
  * GPS coordinates
  */
 public class Location {
+
+    private int x;
+    private int y;
+
+    public Location(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
