02c005f571f4136b8c37bcb19b4387c4a980bd3e
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2019-05-04 16:19:15 +0200

    Testing using date and name in Challenge class - added comments

--- project/src/main/java/projectfiles/Challenge.java
+++ project/src/main/java/projectfiles/Challenge.java
@@ -2,9 +2,13 @@ package projectfiles;
 
 import java.util.Date;
 
+/**
+ * The Challenge that a user can create
+ */
 public class Challenge {
 	private String name;
 	private Date date = new Date();
+	private String description; //TODO fixa den här
 
 	Challenge(String name){
 		this.name = name;
@@ -12,10 +16,7 @@ public class Challenge {
 
 	}
 
-	public Date getDate() {
-		return date;
-	}
-
+	// used for testing
 	@Override
 	public String toString(){
 		return "\nChallenge toString: "+
--- project/src/main/java/projectfiles/Main.java
+++ project/src/main/java/projectfiles/Main.java
@@ -5,6 +5,8 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String args[]) {
+
+		//Code to test the Challenge class
 		Challenge testChallenge = new Challenge("testChallenge");
 		System.out.println(testChallenge.toString());
 	}
