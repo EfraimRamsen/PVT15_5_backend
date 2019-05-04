21c3880fe72ae1543efa19e92755b110abfd4505
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2019-05-04 15:36:47 +0200

    Create empty classes Challenge & Main while listening to Palace & Main - Kent

---
+++ project/src/main/java/projectfiles/Challenge.java
@@ -0,0 +1,13 @@
+package projectfiles;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Challenge {
+	private String name;
+	private Time time; // ej säker på om det är rätt tänkt
+	private Date date;
+
+
+
+}
