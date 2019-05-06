9fce04ae97ac10850b3e0e42fddd5785526f02f9
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2019-05-06 22:26:56 +0200

    Testing JSON import from URL. Problem with mapping JSON values to java variables because of the nested JSON format in the Stockholm API probably.

--- project/src/main/java/projectfiles/FetchJSONFromAPI.java
+++ project/src/main/java/projectfiles/FetchJSONFromAPI.java
@@ -1,9 +1,33 @@
 package projectfiles;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Map;
+
 public class FetchJSONFromAPI {
-	int x = 1;
-	int y = 2;
-	String name = "TEST";
+	private String name;
+	private int x;
+	private int y;
+
+	public FetchJSONFromAPI(String url) throws IOException, URISyntaxException {
+		this.name = name;
+//		this.x = x;
+//		this.y = y;
+
+		URL urlFoReal = new URL(url);
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//		OutdoorGym outdoorGym = mapper.readValue(urlFoReal, OutdoorGym.class);
+//		System.out.println(outdoorGym);
+		Map<String,Object> jsonMap = mapper.readValue(urlFoReal, new TypeReference<Map<String,Object>>(){});
+		System.out.println(jsonMap.toString());
+	}
 
 	public int getX() {
 		return x;
@@ -28,13 +52,5 @@ public class FetchJSONFromAPI {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-
-
-
-
-	//test
-
 
 }
