429e7da87c5f60aa87093b89e74b391cc6d331d8
Jocke007 <jocke.ke@gmail.com>
2019-05-08 11:40:51 +0200

    Add id to places

--- project/src/main/java/projectfiles/FetchJSONFromAPI.java
+++ project/src/main/java/projectfiles/FetchJSONFromAPI.java
@@ -1,13 +1,8 @@
 package projectfiles;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Map;
 
 public class FetchJSONFromAPI {
 	private String name;
@@ -21,12 +16,16 @@ public class FetchJSONFromAPI {
 
 		URL urlFoReal = new URL(url);
 
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//		OutdoorGym outdoorGym = mapper.readValue(urlFoReal, OutdoorGym.class);
-//		System.out.println(outdoorGym);
-		Map<String,Object> jsonMap = mapper.readValue(urlFoReal, new TypeReference<Map<String,Object>>(){});
-		System.out.println(jsonMap.toString());
+//		ObjectMapper mapper = new ObjectMapper();
+//		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+////		OutdoorGym outdoorGym = mapper.readValue(urlFoReal, OutdoorGym.class);
+////		System.out.println(outdoorGym);
+//        OutdoorGym[] nyttGym = mapper.readValue(urlFoReal, OutdoorGym[].class);
+//
+//        System.out.println(nyttGym.toString());
+
+//		Map<String,Object> jsonMap = mapper.readValue(urlFoReal, new TypeReference<Map<String,Object>>(){});
+//		System.out.println(jsonMap.toString());
 	}
 
 	public int getX() {
