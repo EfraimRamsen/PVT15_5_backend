94f9c7a80b83ab63a66cb40cddda55e8098f01a1
Efraim <38281350+efraimramsen@users.noreply.github.com>
2019-05-08 14:26:12 +0200

    Import GSON in pom.xml. Re-write class FetchJSONFromAPI with GSON instead of Jackson (not complete).

--- project/src/main/java/projectfiles/FetchJSONFromAPI.java
+++ project/src/main/java/projectfiles/FetchJSONFromAPI.java
@@ -1,56 +1,45 @@
 package projectfiles;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Map;
+import java.util.HashMap;
 
-public class FetchJSONFromAPI {
-	private String name;
-	private int x;
-	private int y;
+public class FetchJSONFromAPI{
+	HashMap<Integer, OutdoorGym> outdoorGymHashMap = new HashMap<>();
 
-	public FetchJSONFromAPI(String url) throws IOException, URISyntaxException {
-		this.name = name;
-//		this.x = x;
-//		this.y = y;
+	public void parse(){
+		try {
+			URL url = new URL(
+				"http://api.stockholm.se/ServiceGuideService/ServiceUnitTypes/96a67da3-938b-487e-ac34-49b155cb277b/ServiceUnits/json?apikey=52f545a2957c4615a67ac2025ad9795f");
+			InputStreamReader reader = new InputStreamReader(url.openStream());
+			JsonParser parser = new JsonParser();
+			JsonElement rootElement = parser.parse(reader);
+			JsonArray rootAsArray = rootElement.getAsJsonArray();
 
-		URL urlFoReal = new URL(url);
-
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//		OutdoorGym outdoorGym = mapper.readValue(urlFoReal, OutdoorGym.class);
-//		System.out.println(outdoorGym);
-		Map<String,Object> jsonMap = mapper.readValue(urlFoReal, new TypeReference<Map<String,Object>>(){});
-		System.out.println(jsonMap.toString());
+			for(int i = 0; i < rootAsArray.size(); i++){
+				JsonObject position = rootAsArray.get(i).getAsJsonObject().getAsJsonObject("GeographicalPosition");
+				String gymName = rootAsArray.get(i).getAsJsonObject().get("Name").getAsString();
+				Location l = parseLocation(position);
+				String gymDescription = "This is gym no. " + i; //TODO Get real description from API
+				parseGym(i,l,gymName,gymDescription);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public int getX() {
-		return x;
+	public Location parseLocation(JsonObject position){
+		int x = position.get("X").getAsInt();
+		int y = position.get("Y").getAsInt();
+		Location location = new Location(x,y);
+		return location;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void parseGym(int i, Location position, String gymName, String gymDescription){
+		outdoorGymHashMap.put(i, new OutdoorGym(position,gymName,gymDescription));
 	}
 
 }
