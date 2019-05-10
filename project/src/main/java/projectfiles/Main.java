c3788c72367a016b982f0605c085b92020fce303
MichelELL <michel.mottet@hotmail.com>
2019-05-10 09:24:41 +0200

    add springapplication.run to main method. add imports for springframework

--- project/src/main/java/projectfiles/Main.java
+++ project/src/main/java/projectfiles/Main.java
@@ -1,24 +1,20 @@
 package projectfiles;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.sql.Time;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.*;
-import java.util.Iterator;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
 
@@ -29,6 +25,7 @@ public class Main extends SpringBootServletInitializer {
 
     public static void main(String args[]) throws IOException, URISyntaxException {
 
+        SpringApplication.run(Main.class, args);
 /*
         DBManagement dbm = new DBManagement();
         String userName = ("nills");
